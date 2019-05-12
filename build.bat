266c07092424b496c183cb361f779de1f0110367
andy.lv <lvhs@ucacc.com>
2019-05-12 16:01:53 +0800

    uca seata sample...

---
+++ build.bat
@@ -0,0 +1,6 @@
+gradlew.bat ^
+    clean ^
+    uca-springcloud-jpa-seata:uca-business-service:build ^
+    uca-springcloud-jpa-seata:uca-order-service:build ^
+    uca-springcloud-jpa-seata:uca-storage-service:build ^
+    uca-springcloud-jpa-seata:uca-user-service:build
